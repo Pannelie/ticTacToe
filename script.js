e520e60863697452fa3655ba1529f7df428ce97e
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-01-15 13:53:31 +0100

    tic tac toe

--- script.js
+++ script.js
@@ -6,17 +6,17 @@
  */
 let oGameData = {};
 
-window.addEventListener('load', () => {
-    initGlobalObject();
-    if(checkForGameOver() === 1) {
-        console.log("Spelare 1 vann");        
-    } else if(checkForGameOver() === 2) {
-        console.log("Spelare 2 vann");
-    } else if(checkForGameOver() === 3) {
-        console.log("Oavgjort");
-    } else {
-        console.log("Spelet fortsätter");
-    }
+window.addEventListener("load", () => {
+  initGlobalObject();
+  if (checkForGameOver() === 1) {
+    console.log("Spelare 1 vann");
+  } else if (checkForGameOver() === 2) {
+    console.log("Spelare 2 vann");
+  } else if (checkForGameOver() === 3) {
+    console.log("Oavgjort");
+  } else {
+    console.log("Spelet fortsätter");
+  }
 });
 
 /**
@@ -25,103 +25,80 @@ window.addEventListener('load', () => {
  * Funktionen returnerar inte något värde.
  */
 function initGlobalObject() {
+  //Datastruktur för vilka platser som är lediga respektive har brickor
+  //Genom at fylla i här med antingen X eler O kan ni testa era rättningsfunktioner
+  oGameData.gameField = ["", "", "", "", "", "", "", "", ""];
 
-    //Datastruktur för vilka platser som är lediga respektive har brickor
-    //Genom at fylla i här med antingen X eler O kan ni testa era rättningsfunktioner 
-    oGameData.gameField = ['', '', '', '', '', '', '', '', ''];
-    
-    /* Testdata för att testa rättningslösning */
-    //oGameData.gameField = ['X', 'X', 'X', '', '', '', '', '', ''];
-    //oGameData.gameField = ['X', '', '', 'X', '', '', 'X', '', ''];
-    //oGameData.gameField = ['X', '', '', '', 'X', '', '', '', 'X'];
-    //oGameData.gameField = ['', '', 'X', '', 'X', '', 'X', '', ''];
-    //oGameData.gameField = ['X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O'];
+  /* Testdata för att testa rättningslösning */
+  //oGameData.gameField = ['X', 'X', 'X', '', '', '', '', '', ''];
+  //oGameData.gameField = ['X', '', '', 'X', '', '', 'X', '', ''];
+  //oGameData.gameField = ['X', '', '', '', 'X', '', '', '', 'X'];
+  //oGameData.gameField = ['', '', 'X', '', 'X', '', 'X', '', ''];
+  //oGameData.gameField = ['X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O'];
 
-    //Indikerar tecknet som skall användas för spelare ett.
-    oGameData.playerOne = "X";
+  //Indikerar tecknet som skall användas för spelare ett.
+  oGameData.playerOne = "X";
 
-    //Indikerar tecknet som skall användas för spelare två.
-    oGameData.playerTwo = "O";
+  //Indikerar tecknet som skall användas för spelare två.
+  oGameData.playerTwo = "O";
 
-    //Kan anta värdet X eller O och indikerar vilken spelare som för tillfället skall lägga sin "bricka".
-    oGameData.currentPlayer = "";
+  //Kan anta värdet X eller O och indikerar vilken spelare som för tillfället skall lägga sin "bricka".
+  oGameData.currentPlayer = "";
 
-    //Nickname för spelare ett som tilldelas från ett formulärelement,
-    oGameData.nickNamePlayerOne = "";
+  //Nickname för spelare ett som tilldelas från ett formulärelement,
+  oGameData.nickNamePlayerOne = "";
 
-    //Nickname för spelare två som tilldelas från ett formulärelement.
-    oGameData.nickNamePlayerTwo = "";
+  //Nickname för spelare två som tilldelas från ett formulärelement.
+  oGameData.nickNamePlayerTwo = "";
 
-    //Färg för spelare ett som tilldelas från ett formulärelement.
-    oGameData.colorPlayerOne = "";
+  //Färg för spelare ett som tilldelas från ett formulärelement.
+  oGameData.colorPlayerOne = "";
 
-    //Färg för spelare två som tilldelas från ett formulärelement.
-    oGameData.colorPlayerTwo = "";
+  //Färg för spelare två som tilldelas från ett formulärelement.
+  oGameData.colorPlayerTwo = "";
 
-    //Antalet sekunder för timerfunktionen
-    oGameData.seconds = 5;
+  //Antalet sekunder för timerfunktionen
+  oGameData.seconds = 5;
 
-    //Timerns ID
-    oGameData.timerId = null;
+  //Timerns ID
+  oGameData.timerId = null;
 
-    //Från start är timern inaktiverad
-    oGameData.timerEnabled = false;
+  //Från start är timern inaktiverad
+  oGameData.timerEnabled = false;
 
-    //Referens till element för felmeddelanden
-    oGameData.timeRef = document.querySelector("#errorMsg");
+  //Referens till element för felmeddelanden
+  oGameData.timeRef = document.querySelector("#errorMsg");
 }
 
 /**
  * Kontrollerar för tre i rad genom att anropa funktionen checkWinner() och checkForDraw().
- * Returnerar 0 om spelet skall fortsätta, 
+ * Returnerar 0 om spelet skall fortsätta,
  * returnerar 1 om spelaren med ett kryss (X) är vinnare,
  * returnerar 2 om spelaren med en cirkel (O) är vinnare eller
  * returnerar 3 om det är oavgjort.
  * Funktionen tar inte emot några värden.
  */
-function checkForGameOver() {   
-
-}
+function checkForGameOver() {}
 
 // Säg till om ni vill få pseudokod för denna funktion
 // Viktigt att funktionen returnerar true eller false baserat på om den inskickade spelaren är winner eller ej
-function checkWinner(playerIn) {
-
-}
+function checkWinner(playerIn) {}
 
 //Kontrollera om alla platser i oGameData.GameField är fyllda. Om sant returnera true, annars false.
-function checkForDraw() {
-
-}
-
-
+function checkForDraw() {}
 
 // Nedanstående funktioner väntar vi med!
 
-function prepGame() {
+function prepGame() {}
 
-}
+function validateForm() {}
 
-function validateForm() {
-    
-}
+function initiateGame() {}
 
-function initiateGame() {
+function executeMove(event) {}
 
-}
+function changePlayer() {}
 
-function executeMove (event) {
+function timer() {}
 
-}
-
-function changePlayer() {
-
-}
-
-function timer() {
-
-}
-
-function gameOver(result) {
-
-}
+function gameOver(result) {}
