630eadfe6996acb4d6ec5b67e9b476851d9b8513
Bachchan Thapa <you@example.com>
2025-01-16 08:56:41 +0100

    some .js

--- script.js
+++ script.js
@@ -108,12 +108,12 @@ function checkWinner(playerIn) {
   let winCombinations = [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8],
+    [6, 7, 8], // Rows
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8],
+    [2, 5, 8], // Columns
     [2, 4, 6],
-    [0, 4, 8],
+    [0, 4, 8], // Diangonals
   ];
   for (let i = 0; i < winCombinations.length; i++) {
     console.log(winCombinations[i]);
